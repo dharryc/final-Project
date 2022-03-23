86d94fe0d03d3489be09f5afbc5a8ec2f8639367
dharryc <dharrisoncook@gmail.com>
2022-03-23 07:10:31 -0600

    Trying to make a button that will make an instance of a class

---
+++ lib/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace lib;
+public class playerlist
+{
+    public List<playerData> playerBase = new List<playerData>();
+}
+public class playerData
+{
+    public string thing = "no";
+    public void rename()
+    {
+        Random generator = new Random();
+        int yep = generator.Next(0,13);
+        if(yep == 0)
+            thing = "Heyo";
+        else if(yep == 1)
+            thing = "Why tho";
+        else if(yep == 5)
+            thing = "Yee";
+        else if(yep == 12)
+            thing = "Nah bro";
+        else
+            thing = "Oh no, it didn't choose one of the nice ones";
+    }
+}
